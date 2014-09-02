cbe2062834c5aa50309ed209eb4bab539716d542
Kensuke Nagae <kyanny@gmail.com>
2014-09-03 00:57:12 +0900

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+gem 'opml', github: 'fastladder/opml'
+gem 'builder'
---
+++ merge_opml.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+require 'bundler/setup'
+require 'opml'
+require 'builder'
+
+def get_feed file
+  Opml.new(open(file).read).flatten.select { |opml|
+    opml.attributes['xml_url']
+  }
+end
+
+feeds = []
+Dir['*.xml', '*.opml'].each do |file|
+  feeds += get_feed(file)
+end
+
+builder = Builder::XmlMarkup.new indent: 2
+builder.instruct!
+builder.opml version: '1.0' do
+  builder.body do
+    feeds.each do |feed|
+      builder.outline({}.tap {|h| feed.attributes.each{|k,v| h[k.camelize(:lower)] = v }})
+    end
+  end
+end
+puts builder.target!
